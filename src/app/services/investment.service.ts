825fa976040b2970f29f4bf91a0a48e959ebf733
martin.cigarsky <martin.cigarsky@student.tuke.sk>
2023-09-14 11:58:36 +0200

    added delay

--- src/app/services/investment.service.ts
+++ src/app/services/investment.service.ts
@@ -50,11 +50,11 @@ export class InvestmentService {
           let docRef = doc(this.db, 'investments', investment.id);
           let inv: any = investment;
           updateDoc(docRef, inv);
-          setTimeout (() => {
-            console.log(investment.name + " updated");
-          }, 4000 * index);
-          index++;
         })
+        setTimeout (() => {
+          console.log(investment.name + " updated");
+        }, 4000 * index);
+        index++;
       })
     }
 
